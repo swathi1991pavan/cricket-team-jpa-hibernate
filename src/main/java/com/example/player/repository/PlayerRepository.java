5581f32ec42a486542d274dfe4d0207f82cd0e52
swathi <swathi.smiley1991@gmail.com>
2023-03-26 18:53:19 +0000

    solution

--- src/main/java/com/example/player/repository/PlayerRepository.java
+++ src/main/java/com/example/player/repository/PlayerRepository.java
@@ -1,1 +1,20 @@
 // Write your code here
+package com.example.player.repository;
+
+import java.util.*;
+
+import com.example.player.model.Player;
+
+public interface PlayerRepository {
+
+    ArrayList<Player> getPlayers();
+
+    Player getPlayerById(int playerId);
+
+    Player addPlayer(Player player);
+
+    Player updatePlayer(int playerId, Player player);
+
+    void deletePlayer(int playerId);
+}
+
